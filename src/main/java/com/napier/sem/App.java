3e90e58bcef5e8fca49977990f78df28c99161ab
Harris-Taylor <40708546@live.napier.ac.uk>
2025-10-06 13:09:30 +0100

    added to pom file
    Signed-off-by: Harris-Taylor <40708546@live.napier.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App
         // Get a collection from the database
         MongoCollection<Document> collection = database.getCollection("test");
         // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
+        Document doc = new Document("name", "Harris")
                 .append("class", "DevOps")
                 .append("year", "2024")
                 .append("result", new Document("CW", 95).append("EX", 85));
