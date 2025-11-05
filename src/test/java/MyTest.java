ba8167ce64bf94d78da138d7b8bfe0fb1633367b
eloise <40710171@live.napier.ac.uk>
2025-11-05 16:24:12 +0000

    added the tests

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,17 +1,78 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MyTest {
+
+class MyTest
+{
+    // Test 1 does work
     @Test
-    void stringIsCorrect() {
-        String message = "Hello";
-        assertEquals("Hello", message);
+    void unitTest()
+    {
+        assertEquals(5, 5);
     }
+
+    //Test 2 doesnt work
     @Test
-    void additionWorks() {
-        assertEquals(10, 5 + 5);
+    void unitTest2()
+    {
+        assertEquals(5, 4);
     }
-// hi
+
+    // Test 3 :add delta : the maximum allowed difference between expected and actual values for floating point precision
+    @Test
+    void unitTest4()
+    {
+        assertEquals(5.0, 5.01, 0.02);
+    }
+
+    // Test 4 : does a = b
+    @Test
+    void unitTest5()
+    {
+        int[] a = {1, 2, 3};
+        int[] b = {1, 2, 3};
+        assertArrayEquals(a, b);
+    }
+
+    // Test 5 using condition to tell use if it's true
+    @Test
+    void unitTest6()
+    {
+        assertTrue(5 == 5);
+    }
+
+    // Test 6 using condition that says its false
+    @Test
+    void unitTest7()
+    {
+        assertFalse(5 == 4);
+    }
+
+    // Test 7
+    @Test
+    void unitTest8()
+    {
+        assertNull(null);
+    }
+
+    // Test 8 not null( so needs writing )
+    @Test
+    void unitTest9()
+    {
+        assertNotNull("Hello");
+    }
+
+
+    // illustrates how to test if a method throws an exception. By default, any exception
+    // thrown fails a test if no assertThrows matches.
+    @Test
+    void unitTest10()
+    {
+        assertThrows(NullPointerException.class, this::throwsException);
+    }
+
+    void throwsException() throws NullPointerException
+    {
+        throw new NullPointerException();
+    }
 }
-
-
